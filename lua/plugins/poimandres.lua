d5f10add7bf4145b228d6d6348ac845163513031
averrois <yassine.aitrahou0@gmail.com>
2025-12-08 16:43:38 +0100

    fix: update theme to horizon theme

--- lua/plugins/poimandres.lua
+++ lua/plugins/poimandres.lua
@@ -1,20 +1,24 @@
-return {
-	"olivercederborg/poimandres.nvim",
-	name = "poimandres",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("poimandres").setup({
-			bold_vert_split = false,
-			dim_nc_background = false,
-			disable_background = false,
-			disable_float_background = false,
-			disable_italics = false,
-		})
-	end,
+-- return {
+-- 	"olivercederborg/poimandres.nvim",
+-- 	name = "poimandres",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("poimandres").setup({
+-- 			bold_vert_split = false,
+-- 			dim_nc_background = false,
+-- 			disable_background = false,
+-- 			disable_float_background = false,
+-- 			disable_italics = false,
+-- 		})
+-- 	end,
+--
+-- 	-- optionally set the colorscheme within lazy config
+-- 	init = function()
+-- 		vim.cmd("colorscheme poimandres")
+-- 	end,
+-- }
 
-	-- optionally set the colorscheme within lazy config
-	init = function()
-		vim.cmd("colorscheme poimandres")
-	end,
+return {
+  { "akinsho/horizon.nvim", version = "*" },
 }
